d9eaf35cd0f6bb5ab7120d259f165aabf637e466
K.Lopuch <k.lopuch@x-coding.pl>
2026-01-26 21:03:23 +0100

    login

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -36,7 +36,7 @@ export const login = async (req: Request, res: Response) => {
         const user = await User.findOne({ email });
         if (!user) return res.status(400).json({ message: "Benutzer nicht gefunden." });
 
-        const isMatch = await comparePassword(password, user.password);
+        const isMatch = await user.comparePassword(password);
         if (!isMatch) return res.status(400).json({ message: "Falsches Passwort." });
 
         const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET || "secret", { expiresIn: "7d" });
