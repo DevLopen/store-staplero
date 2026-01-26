c90d5b15cd180b155bff57484160db0390742d50
K.Lopuch <k.lopuch@x-coding.pl>
2026-01-26 20:52:51 +0100

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -26,7 +26,7 @@ const Login = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch(`${API_URL}/api/auth/login`, {
+      const res = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
